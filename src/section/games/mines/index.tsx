53e10afbea4d5fbd66016dbb1d54cc483f46eade
TopTrenDev <nikkimoreno7866@gmail.com>
2024-06-25 19:18:14 +0900

    fix: remove banner

--- src/section/games/mines/index.tsx
+++ src/section/games/mines/index.tsx
@@ -12,7 +12,7 @@ import {
   defaulMine,
   minesAmountPresets,
   multiplerArray,
-  token
+  tokens
 } from '@/constants/data';
 import { EMinesStatus } from '@/constants/status';
 import useToast from '@/hooks/use-toast';
@@ -26,7 +26,7 @@ export default function MinesGameSection() {
   const toast = useToast();
   const [betAmount, setBetAmount] = useState(0);
   const [minesAmount, setMinesAmount] = useState(2);
-  const [selectedToken, setSelectedToken] = useState(token[0]);
+  const [selectedToken, setSelectedToken] = useState(tokens[0]);
   const [probabilities, setProbabilities] = useState<number[]>([]);
   const [selectedProbability, setSelectedProbability] = useState<number>(0);
   const [minesStatus, setMinesStatus] = useState(defaulMine.map(() => false));
@@ -34,14 +34,16 @@ export default function MinesGameSection() {
   const [mineStatus, setMineStatus] = useState(EMinesStatus.NONE);
   const dispatch = useAppDispatch();
   const minesState = useAppSelector((state: any) => state.mines);
-  const [mineImages, setMineImages] = useState(Array(defaulMine.length).fill('mystery'));
+  const [mineImages, setMineImages] = useState(
+    Array(defaulMine.length).fill('mystery')
+  );
   const [lastClickedIndex, setLastClickedIndex] = useState<number | null>(null);
   const [clickedIndices, setClickedIndices] = useState<Set<number>>(new Set());
 
   const resetGame = () => {
     setBetAmount(0);
     setMinesAmount(2);
-    setSelectedToken(token[0]);
+    setSelectedToken(tokens[0]);
     setSelectedProbability(0);
     setMinesStatus(defaulMine.map(() => false));
     setIsGameOver(false);
@@ -95,30 +97,34 @@ export default function MinesGameSection() {
     if (isGameOver) return;
     setLastClickedIndex(index);
     setMineStatus(EMinesStatus.CLICKED);
-    setClickedIndices(prev => new Set(prev.add(index)));
+    setClickedIndices((prev) => new Set(prev.add(index)));
     await dispatch(minesActions.rollingMinesgame(index));
     const propability = setTimeout(() => {
-      setSelectedProbability(prev => prev + 1);
+      setSelectedProbability((prev) => prev + 1);
     }, 1800);
     return () => clearTimeout(propability);
   };
 
   const handleMinesGame = () => {
     if (mineStatus === EMinesStatus.NONE) {
-      if (betAmount > 0 && betAmount <= 100 && minesAmount > 0 && minesAmount < 25) {
+      if (
+        betAmount > 0 &&
+        betAmount <= 100 &&
+        minesAmount > 0 &&
+        minesAmount < 25
+      ) {
         setMineStatus(EMinesStatus.START);
         dispatch(
           minesActions.startMinesgame({
             betAmount: Number(betAmount),
             denom: selectedToken.name,
-            betMinesCount: minesAmount,
+            betMinesCount: minesAmount
           })
         );
       } else {
         toast.error('Invalid Token Amount');
       }
-    }
-    else {
+    } else {
       setMineStatus(EMinesStatus.NONE);
       dispatch(minesActions.cashoutgame());
       const gameover = setTimeout(() => {
@@ -129,7 +135,7 @@ export default function MinesGameSection() {
       }, 1000);
       return () => clearTimeout(gameover);
     }
-  }
+  };
 
   useEffect(() => {
     const result = calculateMiningProbabilities(minesAmount, 25) as number[];
@@ -156,7 +162,7 @@ export default function MinesGameSection() {
           setLastClickedIndex(null);
           setIsGameOver(true);
         }
-        setClickedIndices(prev => {
+        setClickedIndices((prev) => {
           const newSet = new Set(prev);
           newSet.delete(lastClickedIndex);
           return newSet;
@@ -176,62 +182,68 @@ export default function MinesGameSection() {
   }, [minesState.gameResult, lastClickedIndex, mineImages]);
 
   useEffect(() => {
-    if (minesState.error !== '')
-      toast.error(minesState.error);
+    if (minesState.error !== '') toast.error(minesState.error);
     resetGame();
   }, [minesState.error]);
 
   return (
-    <ScrollArea className="h-[calc(100vh-64px)] flex">
-      <div className='w-full flex flex-col gap-3'>
-        <div className='flex justify-center items-center'>
-          <div className="flex flex-row scroll-smooth w-[35vw] gap-1.5 overflow-hidden p-1 bg-darkBlue2 rounded-lg mt-5">
+    <ScrollArea className="flex h-[calc(100vh-64px)]">
+      <div className="flex w-full flex-col gap-3">
+        <div className="flex items-center justify-center">
+          <div className="mt-5 flex w-[35vw] flex-row gap-1.5 overflow-hidden scroll-smooth rounded-lg bg-darkBlue2 p-1">
             {probabilities.map((item, index) => (
-              <div key={index}
-                ref={el => {
+              <div
+                key={index}
+                ref={(el) => {
                   if (selectedProbability === index + 1 && el) {
-                    el.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
+                    el.scrollIntoView({
+                      behavior: 'smooth',
+                      block: 'nearest',
+                      inline: 'center'
+                    });
                   }
                 }}
-                className={`text-xs py-1 px-2.5 rounded-lg border border-[#1D1776] bg-dark-blue font-semibold text-gray500 hover:bg-dark-blue ${selectedProbability === index + 1 ? 'bg-purple text-white' : ''}`}
+                className={`rounded-lg border border-[#1D1776] bg-dark-blue px-2.5 py-1 text-xs font-semibold text-gray500 hover:bg-dark-blue ${selectedProbability === index + 1 ? 'bg-purple text-white' : ''}`}
               >
                 x{item > 1000 ? `${(item / 1000).toFixed(2)}k` : item}
               </div>
-            ))
-            }
+            ))}
           </div>
         </div>
         <div className="flex items-center justify-center">
-          {isGameOver &&
+          {isGameOver && (
             <span className="absolute top-16 text-center text-xl font-bold uppercase text-[#df8002]">
-              {(minesState.earned === null ? ("Game over"
-              ) : `WON ${minesState.earned}`)}
+              {minesState.earned === null
+                ? 'Game over'
+                : `WON ${minesState.earned}`}
             </span>
-          }
+          )}
         </div>
-        <div className="flex flex-col gap-12 mt-12">
+        <div className="mt-12 flex flex-col gap-12">
           <div className="relative flex flex-row justify-around">
-            <div className='w-6/12'>
+            <div className="w-6/12">
               {Array.from({ length: 5 }, (_, rowIndex) => (
                 <div className="flex justify-center gap-5" key={rowIndex}>
-                  {defaulMine.slice(rowIndex * 5, (rowIndex + 1) * 5).map((mine, index) => (
-                    <button
-                      className={`group flex items-center justify-center ${minesStatus[rowIndex * 5 + index] ? 'pointer-events-none' : ''} ${mineStatus === EMinesStatus.START ? 'some-start-class' : ''} ${clickedIndices.has(rowIndex * 5 + index) ? 'ripple-animation' : ''}`}
-                      key={index}
-                      onClick={() => {
-                        if (!minesStatus[rowIndex * 5 + index]) {
-                          handleMinesClick(rowIndex * 5 + index);
-                        }
-                      }}
-                      aria-label={`Mine at position ${index + 1}`}
-                    >
-                      <img
-                        src={`/assets/games/mines/${mineImages[rowIndex * 5 + index]}.svg`}
-                        alt={`Mine ${mineImages[rowIndex * 5 + index]}`}
-                        className="group h-20 w-20 hover:transition-all"
-                      />
-                    </button>
-                  ))}
+                  {defaulMine
+                    .slice(rowIndex * 5, (rowIndex + 1) * 5)
+                    .map((mine, index) => (
+                      <button
+                        className={`group flex items-center justify-center ${minesStatus[rowIndex * 5 + index] ? 'pointer-events-none' : ''} ${mineStatus === EMinesStatus.START ? 'some-start-class' : ''} ${clickedIndices.has(rowIndex * 5 + index) ? 'ripple-animation' : ''}`}
+                        key={index}
+                        onClick={() => {
+                          if (!minesStatus[rowIndex * 5 + index]) {
+                            handleMinesClick(rowIndex * 5 + index);
+                          }
+                        }}
+                        aria-label={`Mine at position ${index + 1}`}
+                      >
+                        <img
+                          src={`/assets/games/mines/${mineImages[rowIndex * 5 + index]}.svg`}
+                          alt={`Mine ${mineImages[rowIndex * 5 + index]}`}
+                          className="group h-20 w-20 hover:transition-all"
+                        />
+                      </button>
+                    ))}
                 </div>
               ))}
             </div>
@@ -254,7 +266,6 @@ export default function MinesGameSection() {
                     <DropdownMenu>
                       <DropdownMenuTrigger asChild>
                         <div className="flex cursor-pointer items-center gap-2 uppercase">
-                          <img src={selectedToken.src} className="h-4 w-4" />
                           {selectedToken.name}
                         </div>
                       </DropdownMenuTrigger>
@@ -262,19 +273,21 @@ export default function MinesGameSection() {
                         <DropdownMenuRadioGroup
                           value={selectedToken.name}
                           onValueChange={(value) => {
-                            const newToken = token.find((t) => t.name === value);
+                            const newToken = tokens.find(
+                              (t) => t.name === value
+                            );
                             if (newToken) {
                               setSelectedToken(newToken);
                             }
                           }}
                         >
-                          {token.map((t, index) => (
+                          {tokens.map((t, index) => (
                             <DropdownMenuRadioItem
                               key={index}
                               value={t.name}
                               className="gap-5 uppercase text-white hover:bg-transparent"
                             >
-                              <img src={t.src} className="h-4 w-4" />
+                              {/* <img src={t.src} className="h-4 w-4" /> */}
                               {t.name}
                             </DropdownMenuRadioItem>
                           ))}
@@ -334,13 +347,17 @@ export default function MinesGameSection() {
               </div>
             </div>
             <Button
-              className="w-72 bg-purple py-7 hover:bg-purple flex flex-col"
+              className="flex w-72 flex-col bg-purple py-7 hover:bg-purple"
               onClick={handleMinesGame}
-              disabled={mineStatus !== EMinesStatus.NONE && lastClickedIndex === null}
+              disabled={
+                mineStatus !== EMinesStatus.NONE && lastClickedIndex === null
+              }
             >
               {mineStatus === EMinesStatus.NONE ? 'Start Bet' : 'Cash Out'}
-              <span className='text-sm'>
-                {selectedProbability === 0 ? "" : `$${(probabilities[selectedProbability - 1] * betAmount).toFixed(2)}`}
+              <span className="text-sm">
+                {selectedProbability === 0
+                  ? ''
+                  : `$${(probabilities[selectedProbability - 1] * betAmount).toFixed(2)}`}
               </span>
             </Button>
           </div>
--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -1,10 +1,3 @@
-import { Coin } from '@cosmjs/stargate';
-import { Denom } from 'kujira.js';
-
-export const coinSort = (a: Coin, b: Coin): number =>
-  parseInt(b.amount || '') / 10 ** Denom.from(b.denom || '').decimals -
-  parseInt(a.amount || '') / 10 ** Denom.from(a.denom || '').decimals;
-
 export const appLink = (app: string): string => {
   const domain = window.location.host.split('.').slice(-2).join('.');
   return 'https://' + app + (app === '' ? '' : '.') + domain;
