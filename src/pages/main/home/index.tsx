8232ef3f87e62330d9c671c8a6b2803921354d0b
TopTrenDev <nikkimoreno7866@gmail.com>
2024-06-27 09:22:06 +0900

    fix: landing page

--- src/pages/main/home/index.tsx
+++ src/pages/main/home/index.tsx
@@ -6,14 +6,18 @@ export default function Home() {
     <ScrollArea className="h-[calc(100vh-64px)]">
       <div className="flex h-full flex-col items-center gap-8 p-12">
         <div className="my-[100px] flex flex-col items-center justify-center gap-6">
-          <span className="my-[20px] text-center text-xl font-semibold uppercase text-gray300">
-            THE ORIGINAL CRASH GAME
+          <span className="my-[20px] rounded-full bg-[#14F19514] px-[16px] py-1 text-center text-xl font-normal text-[#14F195]">
+            The Original Crash Game
           </span>
           <div className="flex h-full w-full flex-col items-center justify-between gap-8 text-white">
-            <h1 className="my-[10px] text-[65px] font-semibold text-[#e58929]">
-              solcrash
+            <h1 className="my-[10px] text-center text-[100px] font-semibold text-[#fff]">
+              Fast for everyone to{' '}
+              <h1 className="text-[#14F195]">Win A Prize</h1>
             </h1>
-            <h3 className="my-[10px] ">SOLANA GAMBLING SINCE 2024</h3>
+            <h3 className="my-[10px] w-1/2 text-center">
+              Crash Game that supports experiences for confinience Bankroll,
+              Wagered, and House edge
+            </h3>
             <Link
               to={'/play'}
               className="my-[10px] w-[40vw] cursor-pointer border-none bg-[#c36f18] px-[50px] py-[15px] text-center text-white shadow-md hover:bg-[#c0996e]"
